3e0f19c55f16552a2752842157de76fc13b88b8b
Tobias <tobias-m99@gmx.de>
2020-04-29 18:12:53 +0200

    Added Stop Command

--- JoinMusic/src/de/t0biii/music/commands/CMD_PlayMusic.java
+++ JoinMusic/src/de/t0biii/music/commands/CMD_PlayMusic.java
@@ -6,7 +6,9 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import de.t0biii.music.domain.Music;
 import de.t0biii.music.main.Main;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -20,37 +22,50 @@ public class CMD_PlayMusic implements CommandExecutor {
 
   @Override
   public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-    if (sender.hasPermission("JoinMusic.command.reload")) {
-      if (sender instanceof Player) {
-        Player player = ((Player) sender).getPlayer();
-        if (args.length == 1) {
-          if (args[0].equalsIgnoreCase("reload")) {
+
+    if (sender instanceof Player) {
+      Player player = ((Player) sender).getPlayer();
+      if (args.length == 1) {
+        if (args[0].equalsIgnoreCase("reload")) {
+          if (sender.hasPermission("JoinMusic.command.reload")) {
             try {
               plugin.reloadConfig();
-              player.sendMessage(
-                  ChatColor.DARK_AQUA + plugin.getConfig().getString("messages.reload"));
+              player.sendMessage(plugin.prefix + ChatColor.DARK_AQUA
+                  + plugin.getConfig().getString("messages.reload"));
             } catch (Exception exception) {
               player.sendMessage(plugin.prefix + ChatColor.RED + "Reload failed!");
             }
-          } else
-            sendInstructions(player);
-        } else
+          } else {
+            sender.sendMessage(cmd.getPermissionMessage());
+          }
+        } else if (args[0].equalsIgnoreCase("stop")) {
+          if (sender.hasPermission("JoinMusic.command.stop")) {
+            Music.stop(player);
+            player.sendMessage(plugin.prefix + ChatColor.DARK_AQUA
+                + plugin.getConfig().getString("messages.stop"));
+          } else {
+            sender.sendMessage(cmd.getPermissionMessage());
+          }
+        }else {
           sendInstructions(player);
+        }
       } else
-        sender.sendMessage(plugin.prefix + "This command is not for console!");
-    } else {
-      sender.sendMessage(cmd.getPermissionMessage());
-    }
+        sendInstructions(player);
+    } else
+      sender.sendMessage(plugin.prefix + "This command is not for console!");
+
     return true;
   }
 
   private void sendInstructions(CommandSender sender) {
     sender.sendMessage(
-        ChatColor.GRAY + "======= " + ChatColor.GREEN + "JoinMusic " + ChatColor.GRAY + "=======");
+        ChatColor.GRAY + "======= " + ChatColor.GREEN + plugin.prefix + ChatColor.GRAY + "=======");
     sender.sendMessage(ChatColor.GRAY + "/jm " + ChatColor.GREEN + "reload  " + ChatColor.DARK_GRAY
         + "| " + ChatColor.GREEN + "Config Reload!");
+    sender.sendMessage(ChatColor.GRAY + "/jm " + ChatColor.GREEN + "stop  " + ChatColor.DARK_GRAY
+        + "| " + ChatColor.GREEN + "Stop Song Playing!");    
     sender.sendMessage(
-        ChatColor.GRAY + "======= " + ChatColor.GREEN + "JoinMusic " + ChatColor.GRAY + "=======");
+        ChatColor.GRAY + "======= " + ChatColor.GREEN + plugin.prefix + ChatColor.GRAY + "=======");
   }
 
   public static TabCompleter tabCompleter = new TabCompleter() {
@@ -58,7 +73,10 @@ public class CMD_PlayMusic implements CommandExecutor {
     public List<String> onTabComplete(CommandSender commandSender, Command command, String s,
         String[] strings) {
       if (strings.length == 1) {
-        return Collections.singletonList("reload");
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("reload");
+        list.add("stop");
+        return list;
       }
       return Collections.singletonList(" ");
     }
