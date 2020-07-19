f59f5de38e8394e6bb8bfa8660eaf81539258422
altrisi <altrisi.trillosierra@gmail.com>
2020-07-19 16:24:06 +0200

    Fix double semicolon in an import in Main

--- src/main/java/de/t0biii/music/main/Main.java
+++ src/main/java/de/t0biii/music/main/Main.java
@@ -12,7 +12,7 @@ import de.t0biii.music.listener.HANDLER_SongEndEvent;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
-import org.bukkit.plugin.java.JavaPlugin;;
+import org.bukkit.plugin.java.JavaPlugin;
 import de.t0biii.music.domain.bStatsCostum;
 
 
