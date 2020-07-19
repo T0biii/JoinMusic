df25f12130f32bdd1da273e680dd202eb6a91d33
altrisi <altrisi.trillosierra@gmail.com>
2020-07-19 21:26:24 +0200

    Changed defaults for messages and allowDisabling

--- src/main/java/de/t0biii/music/domain/ConfigManager.java
+++ src/main/java/de/t0biii/music/domain/ConfigManager.java
@@ -19,16 +19,16 @@ public class ConfigManager {
     this.plugin.getConfig().addDefault("options.update-check", true);
     this.plugin.getConfig().addDefault("options.updateinfo", true);
     this.plugin.getConfig().addDefault("options.delaySong", 2);
-    this.plugin.getConfig().addDefault("options.allowDisabling", false);
+    this.plugin.getConfig().addDefault("options.allowDisabling", true);
     this.plugin.getConfig().addDefault("messages.reload", "The reload was successful!");
-    this.plugin.getConfig().addDefault("messages.stop", "&3The song was stopped!");
+    this.plugin.getConfig().addDefault("messages.stop", "&3Stopped playing the song! &7&oYou can disable playing a song on join with &b&o/jm stop");
     this.plugin.getConfig().addDefault("messages.no-permission", "&cYou don't have enough permissions");
     this.plugin.getConfig().addDefault("messages.help.stop", "Stop playing the Song!");
     this.plugin.getConfig().addDefault("messages.help.disableOwn", "Disable playing a song when joining");
     this.plugin.getConfig().addDefault("messages.help.enableOwn", "Enable playing a song when joining");
-    this.plugin.getConfig().addDefault("messages.disabled","&3Disabled playing a song on join");
-    this.plugin.getConfig().addDefault("messages.enabled","&3Enabled playing a song on join");
-    this.plugin.getConfig().addDefault("messages.playing", "&2Started Playing the Song: &a&l%song%");
+    this.plugin.getConfig().addDefault("messages.disabled","&3Disabled playing a song when joining. To enable it again, use &b/jm enable");
+    this.plugin.getConfig().addDefault("messages.enabled","&3Enabled playing a song when joining");
+    this.plugin.getConfig().addDefault("messages.playing", "&2Started Playing the Song: &a&l%song%.&2 You can stop it using &a/jm stop");
     this.plugin.getConfig().addDefault("options.metrics", true);
 
     this.plugin.getConfig().options().copyDefaults(true);
