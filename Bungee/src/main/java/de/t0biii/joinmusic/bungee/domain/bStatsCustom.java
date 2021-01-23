76b2f196b4751c3ec421d0d1a42fff0078ed5b83
Tobias <tobias-m99@gmx.de>
2021-01-24 00:08:10 +0100

    Version 6.2 (#59)
    Co-authored-by: Tobias <T0biii@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- Bungee/src/main/java/de/t0biii/joinmusic/bungee/domain/bStatsCustom.java
+++ Bungee/src/main/java/de/t0biii/joinmusic/bungee/domain/bStatsCustom.java
@@ -3,6 +3,7 @@ package de.t0biii.joinmusic.bungee.domain;
 
 import de.t0biii.joinmusic.bungee.JoinMusicBungee;
 import org.bstats.bungeecord.Metrics;
+import org.bstats.charts.SimplePie;
 
 import java.util.concurrent.Callable;
 
@@ -16,7 +17,7 @@ public class bStatsCustom {
     public void customCharts(Metrics bstats) {
         int delay = plugin.cm.getConfig().getInt("delay");
 
-        bstats.addCustomChart(new Metrics.SimplePie("options_delay", new Callable<String>() {
+        bstats.addCustomChart(new SimplePie("options_delay", new Callable<String>() {
             @Override
             public String call()  {
                 return String.valueOf(delay);
